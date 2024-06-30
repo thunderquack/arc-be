ef604a9aabdde8769c41d393b608e065a86010b4
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-06-30 21:43:39 +0200

    feat: authentication

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 import jwt
 import datetime
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}}) 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_secret_key')
 
 @app.route('/api/login', methods=['POST'])
@@ -16,10 +18,10 @@ def login():
         token = jwt.encode({
             'user': username,
             'exp': datetime.datetime.now(datetime.UTC) + datetime.timedelta(hours=1)
-        }, app.config['SECRET_KEY'])
+        }, app.config['SECRET_KEY'], algorithm="HS256")
         return jsonify({'token': token})
 
     return jsonify({'message': 'Invalid credentials'}), 401
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3000, debug=True)
