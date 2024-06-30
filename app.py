6dce4dca350a5063eb42462b7dc9d49808ac2bcf
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-06-30 21:09:23 +0200

    flask auth

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+import jwt
+import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username == 'user' and password == 'password':
+        token = jwt.encode({
+            'user': username,
+            'exp': datetime.datetime.now(datetime.UTC) + datetime.timedelta(hours=1)
+        }, app.config['SECRET_KEY'])
+        return jsonify({'token': token})
+
+    return jsonify({'message': 'Invalid credentials'}), 401
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
