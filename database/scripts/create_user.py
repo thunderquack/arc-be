0e8a0c94e10cca8d5b31fc0ff0ec35c30a9efb9c
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-07-06 12:10:09 +0200

    abspath correction

--- database/scripts/create_user.py
+++ database/scripts/create_user.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from sqlalchemy.orm import sessionmaker
 from werkzeug.security import generate_password_hash
