e66c11af2bf2732e11278e7b9097753a7de610e7
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-07-07 08:58:27 +0200

    lost decorator

--- routes/utils.py
+++ routes/utils.py
@@ -31,3 +31,25 @@ def token_required(f):
         
         return f(current_user, *args, **kwargs)
     return decorated
+
+def roles_required(*roles):
+    def wrapper(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            data = args[0]  # Данные из декодированного токена
+            user_roles = data.get('roles', [])
+            if not any(role in user_roles for role in roles):
+                return jsonify({'message': 'You do not have the required role to access this resource.'}), 403
+            return f(*args, **kwargs)
+        return decorated
+    return wrapper
+
+def revoke_token(token):
+    try:
+        data = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=["HS256"])
+        exp = data['exp']
+        current_time = datetime.datetime.now(datetime.UTC).timestamp()
+        ttl = exp - current_time
+        # redis_client.set(token, 'revoked', ex=int(ttl))
+    except jwt.InvalidTokenError:
+        pass  # Если токен недействителен, мы просто игнорируем его
