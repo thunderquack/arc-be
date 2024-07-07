634b8408c33e95a85f039240d11cc8a632a50e26
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-07-07 17:07:29 +0200

    rabbitmq url correction

--- messaging/utils.py
+++ messaging/utils.py
@@ -1,6 +1,6 @@
 import pika
 
-RABBITMQ_URL = 'amqp://guest:guest@localhost:5672/'
+RABBITMQ_URL = 'amqp://guest:guest@rabbitmq:5672/'
 
 def get_rabbitmq_connection():
     parameters = pika.URLParameters(RABBITMQ_URL)
