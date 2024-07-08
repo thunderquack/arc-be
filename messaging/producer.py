f215b5cc56164fb945a781a12f3c32023c4a3690
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-07-08 18:14:01 +0200

    page update event sender

--- messaging/producer.py
+++ messaging/producer.py
@@ -1,5 +1,5 @@
 from database.config import DATABASE_URL
-from messaging.utils import get_rabbitmq_connection, declare_queues
+from messaging.utils import get_rabbitmq_connection, declare_queues, PAGE_UPDATE_EVENTS, LOGIN_EVENTS_QUEUE
 import datetime
 
 def send_message_to_queue(queue_name, message):
@@ -12,5 +12,9 @@ def send_message_to_queue(queue_name, message):
         print(f'Failed to send message to RabbitMQ: {str(e)}')
 
 def send_login_event(username):
-    message = f'User {username} logged in at {datetime.datetime.utcnow()}'
-    send_message_to_queue('login_events', message)
+    message = f'User {username} logged in at {datetime.datetime.now(datetime.UTC)}'
+    send_message_to_queue(LOGIN_EVENTS_QUEUE, message)
+
+def send_page_update_event(page_id):
+    message = f'{page_id}'
+    send_message_to_queue(PAGE_UPDATE_EVENTS, message)
--- messaging/utils.py
+++ messaging/utils.py
@@ -1,6 +1,8 @@
 import pika
 
 RABBITMQ_URL = 'amqp://guest:guest@rabbitmq:5672/'
+LOGIN_EVENTS_QUEUE = 'login_events'
+PAGE_UPDATE_EVENTS = 'page_update_events'
 
 def get_rabbitmq_connection():
     parameters = pika.URLParameters(RABBITMQ_URL)
@@ -9,4 +11,5 @@ def get_rabbitmq_connection():
     return connection, channel
 
 def declare_queues(channel):
-    channel.queue_declare(queue='login_events')
+    channel.queue_declare(queue=LOGIN_EVENTS_QUEUE)
+    channel.queue_declare(queue=PAGE_UPDATE_EVENTS)
